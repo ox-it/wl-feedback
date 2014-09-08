9d06e5e5c615c149322c19f7c5cc5d110892fe29
Nick Wilson <nick.wilson@it.ox.ac.uk>
2014-09-08 16:33:19 +0100

    WL-3567 : Use dbWrite method as we don't care about return value

--- src/java/org/sakaiproject/feedback/db/Database.java
+++ src/java/org/sakaiproject/feedback/db/Database.java
@@ -28,7 +28,7 @@ public class Database {
 
         try {
             conn = sqlService.borrowConnection();
-            sqlService.dbInsert(conn, insertReportSql, new String[] {userId, email, siteId, type, title, content}, "id");
+            sqlService.dbWrite(conn, insertReportSql, new String[] {userId, email, siteId, type, title, content});
         } catch (Exception e) {
             logger.error("Failed to insert feedback report.", e);
         } finally {
