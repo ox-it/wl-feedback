21f7559b3f7be43e0a6a6a64065c0412d278e30d
Nick Wilson <nick.wilson@it.ox.ac.uk>
2014-09-17 09:43:51 +0100

    WL-3621 : Contact helpdesk pages shouldnt have dropdown list of maintainers or site contact listed
    Couple of changes here:
    1.   For just the Technical Report, (Adam says) they should not have the drop down list and should not list the site contact so I've deleted these.
    2.   For just the Technical Report, (Adam says) the email should always go to weblearn email.
    So in FeedbackEntityProvider, I've got some common logic for where the email should go for both reports.
    
    ps. the indentation for this file is screwed up/inconsistent.

--- src/java/org/sakaiproject/feedback/tool/entityproviders/FeedbackEntityProvider.java
+++ src/java/org/sakaiproject/feedback/tool/entityproviders/FeedbackEntityProvider.java
@@ -130,7 +130,6 @@ public class FeedbackEntityProvider extends AbstractEntityProvider implements Au
         // The senderAddress can be either picked up from the current user's
         // account, or manually entered by the user submitting the report.
         String senderAddress = null;
-        toAddress = sakaiProxy.getSiteProperty(siteId, Site.PROP_SITE_CONTACT_EMAIL);
 
         if (userId != null) {
             senderAddress = sakaiProxy.getUser(userId).getEmail();
@@ -150,10 +149,7 @@ public class FeedbackEntityProvider extends AbstractEntityProvider implements Au
                     return BAD_RECIPIENT;
                 }
             } else {
-                toAddress = sakaiProxy.getSiteProperty(siteId, Site.PROP_SITE_CONTACT_EMAIL);
-                if (toAddress==null){
-                    toAddress = sakaiProxy.getConfigString(Constants.PROP_TECHNICAL_ADDRESS, null);
-                }
+                toAddress = getToAddress(type, siteId);
             }
         }
         else {
@@ -180,10 +176,7 @@ public class FeedbackEntityProvider extends AbstractEntityProvider implements Au
                 return BAD_REQUEST;
             }
 
-            toAddress = sakaiProxy.getSiteProperty(siteId, Site.PROP_SITE_CONTACT_EMAIL);
-            if (toAddress==null){
-                toAddress = sakaiProxy.getConfigString(Constants.PROP_TECHNICAL_ADDRESS, null);
-            }
+            toAddress = getToAddress(type, siteId);
         }
 
         if (toAddress == null || toAddress.isEmpty()) {
@@ -212,7 +205,21 @@ public class FeedbackEntityProvider extends AbstractEntityProvider implements Au
         }
     }
 
-	private List<FileItem> getAttachments(final Map<String, Object> params) throws Exception {
+    private String getToAddress(String type, String siteId) {
+        String toAddress;
+        if (Constants.CONTENT.equals(type)){
+            toAddress = sakaiProxy.getSiteProperty(siteId, Site.PROP_SITE_CONTACT_EMAIL);
+            if (toAddress==null){
+                toAddress = sakaiProxy.getConfigString(Constants.PROP_TECHNICAL_ADDRESS, null);
+            }
+        }
+        else {
+            toAddress = sakaiProxy.getConfigString(Constants.PROP_TECHNICAL_ADDRESS, null);
+        }
+        return toAddress;
+    }
+
+    private List<FileItem> getAttachments(final Map<String, Object> params) throws Exception {
 
 		final List<FileItem> fileItems = new ArrayList<FileItem>();
 
