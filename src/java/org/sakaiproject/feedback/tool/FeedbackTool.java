e4ff6224e3cbd751ec211e2b22be4285fbba8db4
Matthew Buckett <matthew.buckett@it.ox.ac.uk>
2015-05-12 14:35:17 +0100

    Refactor out our overriding of the site ID.
    This means it’s cleaner to see where we have changed the logic.

--- src/java/org/sakaiproject/feedback/tool/FeedbackTool.java
+++ src/java/org/sakaiproject/feedback/tool/FeedbackTool.java
@@ -68,25 +68,16 @@ public class FeedbackTool extends HttpServlet {
 
         String siteId = sakaiProxy.getCurrentSiteId();
 
-
-        String contactUsSiteId = (String) request.getSession().getAttribute("contact.us.origin.site");
-        if (contactUsSiteId!=null) {
-            siteId = contactUsSiteId;
-        }
-        if (siteId.equals("!error")) { // if site is unavailable then retrieve siteId
-            siteId = request.getParameter("siteId");
-        }
-
-        boolean siteExists = siteService.siteExists(siteId);
+        // Change the site ID if overridden.
+        siteId = overrideSiteId(request, siteId);
 
         Site site = null;
-        if (siteExists){
-            try {
-                site = siteService.getSite(siteId);
-            } catch (IdUnusedException e) {
-                throw new RuntimeException("The site cannot be found with siteId: " + siteId, e);
-            }
+        try {
+            site = siteService.getSite(siteId);
+        } catch (IdUnusedException e) {
+            // This is expected in some cases.
         }
+        boolean siteExists = site != null;
 
         Map<String, String> siteUpdaters = new HashMap<String, String>();
         Map<String, String> emailRecipients = new LinkedHashMap<String, String>();
@@ -150,7 +141,27 @@ public class FeedbackTool extends HttpServlet {
         request.getRequestDispatcher("/WEB-INF/bootstrap.jsp").include(request, response);
     }
 
-	private void setStringAttribute(HttpServletRequest request, String key, String value){
+    /**
+     * This allows the site ID be overridden from the session or request.
+     * @param request The HttpServletRequest
+     * @param siteId The current Site ID.
+     * @return The site ID.
+     */
+    private String overrideSiteId(HttpServletRequest request, String siteId) {
+        // This is set by the portal and is the site ID of the original site that was being accessed
+        // when using a URL like /portal/site/{siteId}/page/contact_us this will contain the site ID.
+        String contactUsSiteId = (String) request.getSession().getAttribute("contact.us.origin.site");
+        if (contactUsSiteId!=null) {
+            siteId = contactUsSiteId;
+        }
+        // When inside the !error site the original URL is put as a request parameter and then passed through.
+        if (siteId.equals("!error")) { // if site is unavailable then retrieve siteId
+            siteId = request.getParameter("siteId");
+        }
+        return siteId;
+    }
+
+    private void setStringAttribute(HttpServletRequest request, String key, String value){
 		request.setAttribute(key, StringEscapeUtils.escapeJavaScript(value));
 	}
 
